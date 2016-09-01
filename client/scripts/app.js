92ee85d7e14cc1e275612452d3e3af930b347b38
jstockwin <jstockwin@gmail.com>
2016-09-01 17:42:41 +0100

    Split up layout and data control into two classes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Section } from './section';
+import { DataControl } from './dataControl';
 import { Portfolio } from './portfolio';
+import { Section } from './section';
 
 const Portfolios = React.createClass({
 	render: function () {
 		return (
-			<Section idQueryLink="/api/portfolios/ids">
-				<Portfolio />
-			</Section>
+			<div>
+				<Section id="home" title="" />
+
+				<Section id="portfolio" title="Our Portfolio">
+					<DataControl idQueryLink="/api/portfolios/ids">
+						<Portfolio />
+					</DataControl>
+				</Section>
+
+				<Section id="testimonials" title="Testimonials" />
+
+				<Section id="services" title="Services" />
+
+				<Section id="team" title="Team" />
+
+				<Section id="contact" title="Contact Us" />
+			</div>
 		);
 	},
 });
 
-ReactDom.render(<Portfolios />, document.getElementById('reactPortfolio'));
+ReactDom.render(<Portfolios />, document.getElementById('app'));
